f74306d65c957820c02d0485de0815b8108ebb5f
cryptolake <dhiadah@gmail.com>
2022-05-16 22:42:26 +0100

    export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Get todos from user."""
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    todos = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                         .format(argv[1]))
+    todos = todos.json()
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(argv[1]))
+    user = user.json()
+    user_dict = {str(user.get('id')): []}
+    for todo in todos:
+        td = {"task": todo.get('title'),
+              "completed": str(todo.get('completed')),
+              "username": user.get('username')}
+        user_dict[str(user.get('id'))].append(td)
+
+    with open(str(user.get('id'))+".json", 'w') as f:
+        json.dump(user_dict, f)
