eef2d52dea316cc5c371491a313be633c6dd0efd
cryptolake <dhiadah@gmail.com>
2022-05-16 23:18:16 +0100

    true or false non str

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,20 +6,17 @@ from sys import argv
 
 if __name__ == "__main__":
     todos = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
-                         .format(argv[1]))
-    todos = todos.json()
+                         .format(argv[1])).json()
+
     user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                        .format(argv[1]))
-    user = user.json()
+                        .format(argv[1])).json()
+
     user_dict = {str(user.get('id')): []}
     for todo in todos:
         td = {"task": todo.get('title'),
-              "completed": str(todo.get('completed')),
+              "completed": todo.get('completed'),
               "username": user.get('username')}
-        try:
-            user_dict.get(str(user.get('id'))).append(td)
-        except Exception:
-            pass
+        user_dict[str(user.get('id'))].append(td)
 
     with open(str(user.get('id'))+".json", 'w') as f:
         json.dump(user_dict, f)
