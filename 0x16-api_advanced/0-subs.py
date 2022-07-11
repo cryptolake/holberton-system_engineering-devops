f6add0a9001830b66b474e9587b749003e4f68ac
cryptolake <dhiadah@gmail.com>
2022-07-11 18:32:35 +0100

    non existant and banned

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,11 +2,17 @@
 """Get subs from reddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Get number of subs."""
-    headers = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"}
-    res = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json', headers=headers, allow_redirects=False)
+    headers = {'User-Agent': "Mozilla/5.0"}
+    res = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json',
+                       headers=headers, allow_redirects=False)
     if res.status_code == 302:
         return 0
     rj = res.json()
-    return (rj['data']['subscribers'])
+    try:
+        subs = rj['data']['subscribers']
+    except Exception:
+        return 0
+    return subs
