a0a0f9f0ab13fffeff51fc787acd6bbf944ff3b3
cryptolake <dhiadah@gmail.com>
2022-12-22 01:58:57 +0100

    Fixed this.. i don't even know how i didn't see how wrong this is

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """Get hot posts from reddit sub."""
-
-
 import requests
 
 
 def count_words(subreddit, word_list, after='', hot_list={}):
     """Get posts of sub."""
-    headers = {'User-Agent': "Mozilla/5.0"}
+    headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)"}
     url = 'https://www.reddit.com'
     res = requests.get(f'{url}/r/{subreddit}/hot.json?after={after}?limit=100',
                        headers=headers, allow_redirects=False)
@@ -19,14 +17,14 @@ def count_words(subreddit, word_list, after='', hot_list={}):
     for post in rj['data']['children']:
         title = post['data']['title'].lower().split(' ')
         for word in title:
-            if word in word_list and hot_list.get(word):
+            if word in word_list:
                 if hot_list.get(word):
                     hot_list[word] += 1
                 else:
-                    hot_list[word] = 0
+                    hot_list[word] = 1
 
     if rj['data']['after'] is None:
-        for word in sorted(hot_list, key=lambda x: x[1]):
-            print(word[0], word[1])
-            
+        for word in sorted(hot_list, key=lambda x: hot_list[x]):
+            print("{}: {}".format(word, hot_list[word]))
+
     return count_words(subreddit, rj['data']['after'], hot_list)
